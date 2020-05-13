4da82cc57e6e65b90ca9044be37ef9c01b71a6c7
Lum Beqiri <lumbeqiri@live.com>
2020-05-13 18:40:09 +0200

    removed api call to jsonplaceholder

--- src/App.vue
+++ src/App.vue
@@ -18,12 +18,7 @@ export default {
 
     }
   
-  },
-  mounted() {
-    axios.get('https://jsonplaceholder.typicode.com/todos/1')
-      .then(response => console.log(response));
-  },
-
+  }
 }
 </script>
 
